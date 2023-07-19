bc4838897eb819f77f729b38eb75a12dfa7ac498
DoJun Park <dojun527@naver.com>
2023-07-19 23:45:10 +0900

    Connnect Django with MySQL

--- badada_backend/settings.py
+++ badada_backend/settings.py
@@ -12,15 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import dotenv
 from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -87,10 +85,12 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'), 
         'PASSWORD': os.environ.get('DB_PASS'), 
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT')
+        'PORT': os.environ.get('DB_PORT'),
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
-
 
 
 # Password validation
