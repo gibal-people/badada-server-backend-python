e91af95aa55add0051608ac691ffee4fbea6f34f
DoJun Park <dojun527@naver.com>
2023-07-20 15:19:24 +0900

    Read db data using Django REST framework

--- badada/views.py
+++ badada/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import Answer
 from .serializers import TestDataSerializer
 
@@ -8,7 +9,8 @@ from .serializers import TestDataSerializer
 def print(request):
     return HttpResponse("This is badada app")
 
+@api_view(['GET'])
 def read(request):
     data = Answer.objects.filter(question_num=1)
     serializer = TestDataSerializer(data, many=True)
-    return HttpResponse(serializer.data)
+    return Response(serializer.data)
