b72a2806fb2abf3c3ea22b45f3b5f591911f6adb
DoJun Park <dojun527@naver.com>
2023-07-21 10:58:10 +0900

    ADD meeting content

--- badada/views.py
+++ badada/views.py
@@ -20,3 +20,12 @@ def answer(request):
     data = Answer.objects.all()
     serializer = AnswerSerializer(data, many=True)
     return Response(serializer.data)
+
+
+
+
+# 상위 몇 프로 (프론트 or 백엔드)
+# 사용자 답변 어떤 형식으로 보내줄지 (프론트 -> 백엔드)
+# 답변 기반으로 추천하는 바다/카테고리/바다 설명/추천하는 이유/나와 맞지 않는 바다 response
+# 결과 바탕으로 mbti_cnt, user_cnt UPDATE
+# 피드백 받을 때 어떤 형식으로 줄건지 (json)
